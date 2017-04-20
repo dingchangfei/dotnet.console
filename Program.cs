99ef569cc0c6f613dbf83259e5173592da324963
dingchangfei <changfei.ding@pfizer.com>
2017-04-20 17:38:05 +0800

    inti

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //测试
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Hello World!");
+            Console.Read();
+            Console.Beep();
+        }
+    }
+}
